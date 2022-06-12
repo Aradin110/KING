1b06b3ff75e91e7b3ec8bd5f44ced67016b672ce
Imtiaz Aradin <97676228+Aradin110@users.noreply.github.com>
2022-06-13 02:20:26 +0500

    Update KING.py

--- KING.py
+++ KING.py
@@ -1,24 +1,20 @@
 import os, platform, time
-try:
-    import requests
-except:
-    os.system('pip install requests')
+os.system('termux-setup-storage')
 os.system('git pull')
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls\033[1;37m")
-    os.system('git pull')
-    try:os.system('mkdir /sdcard/4MBF-DATA')
-    except:pass
-    try:os.system('mkdir /sdcard/4MBF-DATA/OK')
-    except:pass
-    try:os.system('mkdir /sdcard/4MBF-DATA/CP')
-    except:pass
-    try:os.system('mkdir /sdcard/4MBF-DATA/TAP-A2F')
-    except:pass
-    try:os.system('touch .prox.txt')
-        from KING import Subscription
-        Subscription()
+try:os.system('mkdir /sdcard/4MBF-DATA')
+except:pass
+try:os.system('mkdir /sdcard/4MBF-DATA/OK')
+except:pass
+try:os.system('mkdir /sdcard/4MBF-DATA/CP')
+except:pass
+try:os.system('mkdir /sdcard/4MBF-DATA/TAP-A2F')
+except:pass
+try:os.system('touch .prox.txt')
+except:pass
+if __name__ == "__main__":
+        try:
+                __import__("KING").Subscription()
+        except Exception as e:
+                exit(str(e))
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
